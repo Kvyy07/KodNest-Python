b1c16f7cf0e0461a1b6ca195cb559d1c813e12d4
Kaviyarasan <kavimathi0720@gmail.com>
2026-08-08 16:22:50 +0530

    type_conver.py

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/type_conver.py
@@ -0,0 +1,15 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+
+age = int(age_text)
+print(age)
+print(type(age))
+
+course_fee = float(course_fee_text)
+print(course_fee)
+print(type(course_fee))
+
+attempts_text = str(attempts)
+print(attempts_text)
+print(type(attempts_text))
