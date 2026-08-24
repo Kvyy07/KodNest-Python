14b0d5c1904a73b1b267b98a1bfd631da0ec82df
Kaviyarasan <kavimathi0720@gmail.com>
2026-08-24 23:17:09 +0530

    class constructor

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/m1/class_constructor.py
@@ -0,0 +1,21 @@
+class student:
+    def __init__(self):
+        self.name =""
+        self.register_number =""
+    def display(self):
+        print("Name:", self.name)
+        print("Register No:", self.register_number)
+
+s1 = student()
+s2 = student()   
+
+s1.name = "kavyy"
+s1.register_number = "90"
+
+s2.name = "Archuuu"
+s2.register_name = "91"
+
+
+s1.display()
+s2.display()
+        
